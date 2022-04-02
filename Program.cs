06a36d22b501de970786bb41d6d32f31e70d45f4
Kauã <kau.lira@yahoo.com>
2022-04-01 21:31:12 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int numero;
+
+Console.Write("Digite um número:");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero < 0)
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("Você digitou um número negativo");
+    Console.Beep();
+}
+
+Console.ResetColor();
